46efa25d755539fa2b25716fec4a7d279b75215e
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2022-03-24 11:16:01 +0530

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -15,16 +15,13 @@ pacman --noconfirm -Sy archlinux-keyring
 timedatectl set-ntp true
 sfdisk /dev/sda << EOF
 ,500m
-,2g
 ;
 EOF
 mkfs.fat -F 32 /dev/sda1
-mkswap /dev/sda2
-mkfs.ext4 /dev/sda3
-mount /dev/sda3 /mnt
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
-swapon /dev/sda2
 pacstrap /mnt base linux linux-firmware efibootmgr grub networkmanager base-devel
 genfstab -U /mnt >> /mnt/etc/fstab
 sed '1,/^# Step 2$/d' `basename $0` > /mnt/arch_install2.sh
