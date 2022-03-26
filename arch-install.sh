d1128b742cf0f023b44b76c9aa68177cbda1a939
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2022-03-26 14:11:26 +0530

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -1,22 +1,17 @@
-⁷#!/bin/bash
-printf= "
--------------------------------------------------------------------------------------------------------------------
-| __        __   _                            _____          _             _          ___           _        _ _  |
-| \ \      / /__| | ___ ___  _ __ ___   ___  |_   _|__      / \   _ __ ___| |__      |_ _|_ __  ___| |_ __ _| | | |
-|  \ \ /\ / / _ \ |/ __/ _ \|  _   _ \ / _ \   | |/ _ \    / _ \ |  __/ __|  _ \      | ||  _ \/ __| __/ _  | | | |
-|   \ V  V /  __/ | (_| (_) | | | | | |  __/   | | (_) |  / ___ \| | | (__| | | |     | || | | \__ \ || (_| | | | |
-|    \_/\_/ \___|_|\___\___/|_| |_| |_|\___|   |_|\___/  /_/   \_\_|  \___|_| |_|    |___|_| |_|___/\__\__ _|_|_| |
-|                                                                                                                 |
--------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+clear
+echo "
+_ _ _ ____ _    ____ ____ _  _ ____    ___ ____    ____ ____ ____ _  _    _ _  _ ____ ___ ____ _    _    
+| | | |___ |    |    |  | |\/| |___     |  |  |    |__| |__/ |    |__|    | |\ | [__   |  |__| |    |    
+|_|_| |___ |___ |___ |__| |  | |___     |  |__|    |  | |  \ |___ |  |    | | \| ___]  |  |  | |___ |___ 
+
 "
-echo "ENTER HOSTNAME:"
-read "hostname"
-echo "USERNAME:"
-read "username"
-echo "USER PASSWORD"
-read "user_password"
-echo "ROOT PASSWORD"
-read "root_password"
+
+read -p "Please enter hostname: " hostname
+read -p "Please enter username: " username
+read -p "Please enter user password: " user_password
+read -p "Please enter root password: " root_password
+clear
 
 # Step 1
 loadkeys us
@@ -34,6 +29,7 @@ mkfs.ext4 /dev/sda2
 mount /dev/sda2 /mnt
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
+clear
 pacstrap /mnt base linux linux-firmware efibootmgr grub networkmanager sed nano 
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "echo $hostname > /etc/hostname" >> /mnt/temp.sh
@@ -41,10 +37,6 @@ echo "useradd -m -G wheel -s /bin/bash $username" >> /mnt/temp.sh
 echo "echo \"$username ALL=(ALL:ALL) ALL\" >> /etc/sudoers" >> /mnt/temp.sh
 echo "echo $username:$user_password | chpasswd" >> /mnt/temp.sh
 echo "echo root:$root_password | chpasswd" >> /mnt/temp.sh
-sed -i '38d' /etc/systemd/system/getty.target.wants/getty@tty1.service
-echo "[Service]" >> /etc/systemd/system/getty.target.wants/getty@tty1.service
-echo "ExecStart=" >> /etc/systemd/system/getty.target.wants/getty@tty1.service
-echo "ExecStart=-/sbin/agetty -a $username %I" '$TERM' >> /etc/systemd/system/getty.target.wants/getty@tty1.service
 chmod u+x /mnt/temp.sh 
 sed '1,/^# Step 2$/d' `basename $0` > /mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
@@ -65,25 +57,29 @@ sed -i 's/quiet/pci=noaer/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable NetworkManager.service
-location=/home/$username/arch_install3.sh
-sed '1,/^# Step 3$/d' arch_install2.sh > $location
-chown $username:$username $location
-chmod +x $location
-su -c $location -s /bin/sh $username
-exit 
-sudo reboot now
+location= /home/arch_install3.sh
+sed '1,/^# Step 3$/d' arch_install2.sh > /home/arch_install3.sh
+chmod +x /home/arch_install3.sh
+exit
+clear
+echo "INSTALLATION COMPLETED PLEASE RESTART"
+sleep 10
 
 # Step 3
+sudo sed -i '38d' /etc/systemd/system/getty.target.wants/getty@tty1.service
+sudo echo "[Service]" >> /etc/systemd/system/getty.target.wants/getty@tty1.service
+sudo echo "ExecStart=" >> /etc/systemd/system/getty.target.wants/getty@tty1.service
+sudo echo "ExecStart=-/sbin/agetty -a $username %I" '$TERM' >> /etc/systemd/system/getty.target.wants/getty@tty1.service
 pacman -Ss --noconfirm fonts ttf
 pacman -S --noconfirm ttf-dejavu pango i3 dmenu ffmpeg jq curl \
-                dmenu xorg-server xorg-xinit alacritty pavucontrol \
+        dmenu xorg-server xorg-xinit alacritty pavucontrol \
         light picom rofi git nautilus firefox gparted base-devel\
         gnome-boxes arandr feh bluez bluez-utils libreoffice \
         mpv neofetch qbittorrent code xorg-xprop sxiv nano \
-        pulseaudio sysstat openssh
+        pulseaudio sysstat openssh xorg
+clear     
 rfkill unblock all
 echo "exec i3 " >> ~/.xinitrc
-systemctl enable NetworkManager.service
 systemctl start bluetooth.service
 systemctl enable bluetooth.service
 echo "Enter Username "
@@ -95,10 +91,6 @@ sudo chown -R $username:$username ./yay-git
 cd yay-git
 makepkg -si
 yay -S --noconfirm pfetch 
-cd /home/$username
-git clone https://aur.archlinux.org/zoom.git
-cd zoom
-makepkg -si
 sudo chmod +s /usr/bin/light
 sudo curl -sL "https://raw.githubusercontent.com/Edesem/bluetooth-connect-script/main/bcn" -o /usr/local/bin/bluez
 sudo chmod +x /usr/local/bin/bluez
@@ -117,13 +109,8 @@ cd
 
 ### Manual processes
 
-## auto login
-#          sudo nano /etc/systemd/system/getty.target.wants/getty@tty1.service
-#          ExecStart=
-#          ExecStart=-/sbin/agetty -a $username %I $TERM 
-
-## Auto start i3 on boot 
-#          sudo nano /etc/profile 
+## Auto start i3wm on boot 
+#          sudo nano /etc/profile  
 #          if [["$(tty)"=='/dev/tty1']]; then
 #          exec startx
 #          fi
